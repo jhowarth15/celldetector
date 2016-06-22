a847b8a869194c380056de2c525e12f7a5ecc7ae
Joshua Howarth <joshuahowarth@joshuas-mbp.home>
2016-06-22 16:31:19 +0100

    Upload server is awake for more than one file

--- pythonclassifier/upload.py
+++ pythonclassifier/upload.py
@@ -1,15 +1,17 @@
 import socket
 import struct
+import os, os.path
 
 HOST = "127.0.0.1"
 PORT = 9999
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(5)
 
-print('SERVER STARTED RUNNING')
+print('IMAGE UPLOAD SERVER STARTED RUNNING')
 
 while True:
     client, address = s.accept()
@@ -17,7 +19,12 @@ while True:
     while len(buf) < 4:
         buf += client.recv(4 - len(buf))
     size = struct.unpack('!i', buf)[0]
-    with open('/Users/joshuahowarth/dev/celldetector/pythonclassifier/uploaded_images/image.png', 'wb') as f:
+
+    DIR = os.getcwd()+'/uploaded_images/'
+    frame_count = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+    print frame_count
+
+    with open('/Users/joshuahowarth/dev/celldetector/pythonclassifier/uploaded_images/image_%d.png' % frame_count, 'wb') as f:
         while size > 0:
             data = client.recv(1024)
             f.write(data)
@@ -25,7 +32,7 @@ while True:
     print('Image Saved')
     client.sendall('Image Received')
     client.close()
-    break
+
 
 s.close()
-print('SOCKET CLOSED')
+print('SERVER AND SOCKET CLOSED')
